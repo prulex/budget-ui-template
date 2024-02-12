4d97958c6db586a1ff7afc79bcc7eca7414277ee
siro <siro.beck@lernen.siw.swiss>
2024-02-12 15:01:18 +0100

    Progressbar und az

--- src/app/expense/expense-list/expense-list.component.ts
+++ src/app/expense/expense-list/expense-list.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 import { addMonths, set } from 'date-fns';
-import { ModalController } from '@ionic/angular';
+import { ModalController, RefresherCustomEvent } from '@ionic/angular';
 import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
-import { Expense } from '../../shared/domain';
+import { CategoryCriteria, Expense, ExpenseUpsertDto, ExpenseCriteria, SortOption } from '../../shared/domain';
+import { CategoryService } from '../../category/category.service';
+import { ToastService } from '../../shared/service/toast.service';
+import { finalize, from, groupBy, mergeMap, toArray } from 'rxjs';
+import { formatPeriod } from '../../shared/period';
+import { ExpenseService } from '../expense.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 interface ExpenseGroup {
   date: string;
@@ -14,12 +20,77 @@ interface ExpenseGroup {
 })
 export class ExpenseListComponent {
   date = set(new Date(), { date: 1 });
+  expenseGroups: ExpenseGroup[] | null = null;
+  readonly initialSort = 'name,asc';
+  lastPageReached = false;
+  loading = false;
+  readonly searchForm: FormGroup;
+  searchCriteria: ExpenseCriteria = { page: 0, size: 25, sort: this.initialSort };
 
-  constructor(private readonly modalCtrl: ModalController) {}
+  readonly sortOptions: SortOption[] = [
+    { label: 'Created at (newest first)', value: 'createdAt,desc' },
+    { label: 'Created at (oldest first)', value: 'createdAt,asc' },
+    { label: 'Date (newest first)', value: 'date,asc' },
+    { label: 'Date (oldest first)', value: 'date,desc' },
+    { label: 'Name (A-Z)', value: 'name,asc' },
+    { label: 'Name (Z-A)', value: 'name,desc' },
+  ];
+
+  constructor(
+    private readonly modalCtrl: ModalController,
+    private readonly expenseService: ExpenseService,
+    private readonly toastService: ToastService,
+    private readonly formBuilder: FormBuilder,
+  ) {
+    this.searchForm = this.formBuilder.group({ name: [], sort: [this.initialSort] });
+  }
 
   addMonths = (number: number): void => {
     this.date = addMonths(this.date, number);
   };
+  ionViewDidEnter(): void {
+    this.loadExpenses();
+  }
+
+  private loadExpenses(next: () => void = () => {}): void {
+    this.searchCriteria.yearMonth = formatPeriod(this.date);
+    if (!this.searchCriteria.categoryIds?.length) delete this.searchCriteria.categoryIds;
+    if (!this.searchCriteria.name) delete this.searchCriteria.name;
+    this.loading = true;
+    const groupByDate = this.searchCriteria.sort.startsWith('date');
+    this.expenseService
+      .getExpense(this.searchCriteria)
+      .pipe(
+        finalize(() => (this.loading = false)),
+        mergeMap((expensePage) => {
+          this.lastPageReached = expensePage.last;
+          next();
+          if (this.searchCriteria.page === 0 || !this.expenseGroups) this.expenseGroups = [];
+          return from(expensePage.content).pipe(
+            groupBy((expense) => (groupByDate ? expense.date : expense.id)),
+            mergeMap((group) => group.pipe(toArray())),
+          );
+        }),
+      )
+      .subscribe({
+        next: (expenses: Expense[]) => {
+          const expenseGroup: ExpenseGroup = {
+            date: expenses[0].date,
+            expenses: this.sortExpenses(expenses),
+          };
+          const expenseGroupWithSameDate = this.expenseGroups!.find((other) => other.date === expenseGroup.date);
+          if (!expenseGroupWithSameDate || !groupByDate) this.expenseGroups!.push(expenseGroup);
+          else
+            expenseGroupWithSameDate.expenses = this.sortExpenses([
+              ...expenseGroupWithSameDate.expenses,
+              ...expenseGroup.expenses,
+            ]);
+        },
+        error: (error) => this.toastService.displayErrorToast('Could not load expenses', error),
+      });
+  }
+
+  private sortExpenses = (expenses: Expense[]): Expense[] => expenses.sort((a, b) => a.name.localeCompare(b.name));
 
   async openModal(expense?: Expense): Promise<void> {
     const modal = await this.modalCtrl.create({
@@ -29,5 +100,15 @@ export class ExpenseListComponent {
     modal.present();
     const { role } = await modal.onWillDismiss();
     console.log('role', role);
+  }
+
+  reloadExpense($event?: any): void {
+    this.searchCriteria.page = 0;
+    this.loadExpenses(() => ($event ? ($event as RefresherCustomEvent).target.complete() : {}));
+  }
+  isSameMonth(dateString: string): boolean {
+    const currentDate = this.date;
+    const expenseDate = new Date(dateString);
+    return currentDate.getFullYear() === expenseDate.getFullYear() && currentDate.getMonth() === expenseDate.getMonth();
   }
 }
